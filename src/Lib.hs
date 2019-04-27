db0259582594b346e27d27bdade70dd00e9e8dbf
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-04-27 19:37:55 +0200

    Creating first functions.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,16 @@ module Lib
     ) where
 
 someFunc2 :: IO ()
-someFunc2 = putStrLn "someFunc2"
+--someFunc2 = putStrLn "hmm"
+--someFunc2 = print (doubleUs 28 88 + doubleMe 123)
+someFunc2 = print conanO'Brien
+
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+    then x
+    else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Connan O'Brien!"
