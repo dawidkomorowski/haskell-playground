e63b8ff1561cfb7d34392e8e42e2469faa860e52
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-05-01 20:08:59 +0200

    Removed no longer needed tracing.

--- src/BinarySearchTddProblem.hs
+++ src/BinarySearchTddProblem.hs
@@ -1,7 +1,5 @@
 module BinarySearchTddProblem (binarySearch) where
 
-import Debug.Trace
-
 binarySearch value sortedList = binarySearchInternal value sortedList 0
 
 centralIndex sortedList = (length sortedList) `div` 2
@@ -12,9 +10,7 @@ right sortedList = drop (rightFromCentralIndex sortedList) sortedList
 
 
 binarySearchInternal value sortedList indexOffset = 
-    trace ("Trace: value " ++ show value ++ " sortedList " ++ show sortedList ++ " indexOffset " ++ show indexOffset) (
     if null sortedList then -1
     else if centralElement sortedList == value then indexOffset + centralIndex sortedList
     else if centralElement sortedList > value then binarySearchInternal value (left sortedList) 0
     else binarySearchInternal value (right sortedList) (indexOffset + rightFromCentralIndex sortedList)
-    )
